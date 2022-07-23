0435d3e6bab6a4d78c5cfdc2e8d54ef76bec5b73
pequito193 <vitorspequito@gmail.com>
2022-07-23 13:37:29 +0100

    Fix design choices and certain colours

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ function operate(operator, firstNumber, secondNumber) {
     let result = operator(firstNumber, secondNumber);
     return result;
 }
+
+function writeOnScreen() {
+    const onScreen = document.querySelector('.screen')
+    onScreen.textContent = 
+}
+
+// Button to select the numbers
+const button = document.querySelectorAll('.number')
+button.addEventListener('click', writeOnScreen)
