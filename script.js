40d8fbbfada05de38a8fbfcc73318853935c6e49
pequito193 <vitorspequito@gmail.com>
2022-07-26 10:51:41 +0100

    Fix screen overflow and button styling

--- script.js
+++ script.js
@@ -19,30 +19,24 @@ function operate(operator, firstNumber, secondNumber) {
     return result;
 }
 
+
+// Button to reset the screen
 const clear = document.querySelector('.clear')
 const screen = document.querySelector('.screen')
 clear.addEventListener('click', e => {
     screen.textContent = ''
 })
 
+
 // Button to select the numbers
 const button = document.querySelectorAll('.button')
 button.forEach(button => button.addEventListener('click', e => {
     const onScreen = document.querySelector('.screen')
     onScreen.textContent = onScreen.textContent + button.textContent
-    console.log(onScreen.textContent.length)
-    if (onScreen.textContent.length >= 20) {
-        button.removeEventListener('click', e => {
-            const onScreen = document.querySelector('.screen')
-            onScreen.textContent = onScreen.textContent + button.textContent
-        })
-    }
 }))
 
-const onScreen = document.querySelector('.screen')
-if (onScreen.textContent.length >= 20) {
-        button.removeEventListener('click', e => {
-            const onScreen = document.querySelector('.screen')
-            onScreen.textContent = onScreen.textContent + button.textContent
-    })
-}
+
+const equalSign = document.querySelector('.equal-sign')
+equalSign.addEventListener('click', e => {
+
+})
